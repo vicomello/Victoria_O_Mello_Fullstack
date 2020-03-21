e89363b086bbbc3a6d0bd0cc14e004ca6a8c775f
Victoria <vic.oldemburgo@gmail.com>
2020-03-21 18:23:25 -0300

    primeira versão de divisores feita

---
+++ application.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+import csv
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def calcular():
+    # Abrindo o arquivo em que serão salvos os resultados
+    file = open("data.csv", "a", newline='')
+    fieldnames = ['numero', 'resultado']
+    writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+    divisores = list()
+    if request.method == "GET":
+        return render_template("page.html")
+    elif request.method == "POST":
+        # Pegando o número do digitado no formulário e calculando seus divisores
+        number = request.form.get("number")
+        if number.isdigit():
+            x = int(number)
+            if x > 0:
+                for i in range(1, (x + 1)):
+                    if (x / i).is_integer():
+                        divisores.append(i)
+                if len(divisores) == 2:
+                    message = ("{} é primo".format(x))
+                else:
+                    message = ("A lista de divisores é {}".format(divisores))
+                # Escrevendo as informações no arquivo csv
+                writer.writerow({'numero': x, 'resultado': divisores})
+            else:
+                message = "Você deve digitar um número natural positivo!"
+        else:
+            message = "Você deve digitar um número natural!"
+
+        file.close()
+        return render_template("page.html", message=message)
